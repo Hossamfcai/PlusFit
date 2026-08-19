35b5a2dce837822c3804bab64262d75e81fa946e
Hossam Ibrahim <hossamibrahim.fcai23@gmail.com>
2026-08-19 20:33:37 +0300

    add validation to the form and on submited send mail by integrated with email js that integrated with company's email

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,4 @@
-console.log("done");
+emailjs.init("0Ne1LTKR2ABJISp_e");
 const formEl = document.getElementById("formContainer");
 const notification = document.getElementById("notification");
 
@@ -11,9 +11,41 @@ formEl.addEventListener("submit", (e) => {
   const data = Object.fromEntries(formData.entries());
   console.log(data);
 
+  emailjs
+    .sendForm("service_1lqy2ak", "template_54ei0qb", formEl)
+    .then(() => {
+      displayNotification({
+        message: "Submitted successfully!",
+
+        iconType: "check-circle",
+      });
+      formEl.reset();
+    })
+    .catch((error) => {
+      console.error("Failed to send email:", error);
+      displayNotification({
+        message: "Failed to send email try again later",
+
+        iconType: "x-circle",
+      });
+    });
+});
+
+function displayNotification(notificationData) {
   clearTimeout(hideTimeout);
   clearTimeout(resetTimeout);
+  const icon = document.createElement("i");
+  icon.classList.add(
+    notificationData.iconType == "x-circle" ? "text-red-600" : "text-green-600",
+    "mx-2",
+    "bi",
+    `bi-${notificationData.iconType}-fill`,
+  );
 
+  const messageEL = document.createElement("span");
+  messageEL.innerHTML = notificationData.message;
+
+  notification.append(icon, messageEL);
   notification.classList.remove("hide");
   notification.classList.add("show");
 
@@ -23,6 +55,7 @@ formEl.addEventListener("submit", (e) => {
 
     resetTimeout = setTimeout(() => {
       notification.classList.remove("hide");
+      notification.replaceChildren();
     }, 300);
   }, 2000);
-});
+}
