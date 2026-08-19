8ffd2988eb0a8d4738cf45f781882ee7ed8fecf9
Hossam Ibrahim <hossamibrahim.fcai23@gmail.com>
2026-08-19 14:29:11 +0300

    complete contact info and contact form

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,28 @@
 console.log("done");
+const formEl = document.getElementById("formContainer");
+const notification = document.getElementById("notification");
+
+let hideTimeout = null;
+let resetTimeout = null;
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(formEl);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+
+  clearTimeout(hideTimeout);
+  clearTimeout(resetTimeout);
+
+  notification.classList.remove("hide");
+  notification.classList.add("show");
+
+  hideTimeout = setTimeout(() => {
+    notification.classList.remove("show");
+    notification.classList.add("hide");
+
+    resetTimeout = setTimeout(() => {
+      notification.classList.remove("hide");
+    }, 300);
+  }, 2000);
+});
